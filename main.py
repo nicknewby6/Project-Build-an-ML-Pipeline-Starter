dc7365fb8c8bbf7b5e50d11617169f37f3f4e21a
Nick Newby <nicknewby6@gmail.com>
2025-04-30 14:15:34 -0600

    Step 3: Data testing

--- main.py
+++ main.py
@@ -51,17 +51,32 @@ def go(config: DictConfig):
             )
 
         if "basic_cleaning" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
-
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(),'src','basic_cleaning'),
+                'main',
+                parameters={
+                    'input_artifact':'sample.csv:latest',
+                    'output_artifact': 'clean_sample',
+                    'output_type': 'cleaned_data',
+                    'output_description': 'Data with outliers and null values removed',
+                    'min_price': config['etl']['min_price'],
+                    'max_price': config['etl']['max_price']
+                }
+            )
+            
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(),'src','data_check'),
+                'main',
+                parameters={
+                    'csv':'clean_sample:latest',
+                    'ref': 'clean_sample:reference',
+                    'kl_threshold': config['data_check']['kl_threshold'],
+                    'min_price': config['etl']['min_price'],
+                    'max_price': config['etl']['max_price']
 
+                }
+            )
         if "data_split" in active_steps:
             ##################
             # Implement here #
